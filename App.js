1eb7d8ebad3977ff5fd07ce18506c1584b6a319b
Zhuo Chen <zhuochen83@gmail.com>
2017-06-27 12:35:22 +1200

    First commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements'
+import JobListContainer from './lib/components/JobListContainer';
+import CreateJob from './lib/components/CreateJob';
+import { JobStack } from './lib/router'
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <JobStack/>      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
